576802e4fd48ceb26c06b9ab441600d258681c97
unknown <3702740@qq.com>
2019-11-30 21:20:22 +0800

    update

--- script/packajax.js
+++ script/packajax.js
@@ -1,23 +1,23 @@
 //////////使用前必须先引用api.js
 /////////////正式环境地址///////////////////////
-//var serviceUrl = 'https://www.zxj888.cn:8443'//正式环境
-//var serviceUrl02 = 'https://www.zxj888.cn:9443'//正式环境
-//var upFileUrl="https://www.zxj888.cn:8443/upFile";//上传文件url
-//var webSocketAgreement='wss';//webSocket协议
-//var webSocketUrl='zxj888.cn'//webSocket域名
-//var webSocketPort=9443;//webSocket端口
-//var webSocketPath='zxj/websocket';//webSocket域名后面那部分路径
-///////////////////正式环境地址end///////////////
-
-///////////////////////////////开发环境地址//////// //////
-var serviceUrl = 'https://www.zxjtest.xyz'//开发环境
-var serviceUrl02 = 'https://www.zxjtest.xyz:9443'//开发环境
-var upFileUrl="https://www.zxjtest.xyz/upFile";//上传文件url
+var serviceUrl = 'https://www.zxj888.cn:8443'//正式环境
+var serviceUrl02 = 'https://www.zxj888.cn:9443'//正式环境
+var upFileUrl="https://www.zxj888.cn:8443/upFile";//上传文件url
 var webSocketAgreement='wss';//webSocket协议
-var webSocketUrl='zxjtest.xyz'//webSocket域名
+var webSocketUrl='zxj888.cn'//webSocket域名
 var webSocketPort=9443;//webSocket端口
 var webSocketPath='zxj/websocket';//webSocket域名后面那部分路径
-/////////////////////////开发环境地址end///////////////
+///////////////////正式环境地址end///////////////
+
+/////////////////////////////////开发环境地址//////// //////
+//var serviceUrl = 'https://www.zxjtest.xyz'//开发环境
+//var serviceUrl02 = 'https://www.zxjtest.xyz:9443'//开发环境
+//var upFileUrl="https://www.zxjtest.xyz/upFile";//上传文件url
+//var webSocketAgreement='wss';//webSocket协议
+//var webSocketUrl='zxjtest.xyz'//webSocket域名
+//var webSocketPort=9443;//webSocket端口
+//var webSocketPath='zxj/websocket';//webSocket域名后面那部分路径
+///////////////////////////开发环境地址end///////////////
 
 //////////////////////////////////
 //var serviceUrl = 'http://ybhm6m.natappfree.cc/server'//本地开发原件
